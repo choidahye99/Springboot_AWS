7eae1c4d1583c273bd53702572d011eacafde90d
최다혜 <dhc0404@naver.com>
2023-01-24 10:20:32 +0900

    update

--- freelec-springboot-webservice/src/main/java/com/project/freelecspringbootwebservice/service/posts/PostsService.java
+++ freelec-springboot-webservice/src/main/java/com/project/freelecspringbootwebservice/service/posts/PostsService.java
@@ -1,7 +1,9 @@
 package com.project.freelecspringbootwebservice.service.posts;
 
+import com.project.freelecspringbootwebservice.domain.posts.Posts;
 import com.project.freelecspringbootwebservice.domain.posts.PostsRepository;
 import com.project.freelecspringbootwebservice.web.dto.PostSaveRequestDto;
+import com.project.freelecspringbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,4 +19,16 @@ public class PostsService {
     public Long save(PostSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalAccessError("해당 게시글이 없습니다. id="+id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+
 }
--- freelec-springboot-webservice/src/main/java/com/project/freelecspringbootwebservice/web/PostsApiController.java
+++ freelec-springboot-webservice/src/main/java/com/project/freelecspringbootwebservice/web/PostsApiController.java
@@ -2,10 +2,9 @@ package com.project.freelecspringbootwebservice.web;
 
 import com.project.freelecspringbootwebservice.service.posts.PostsService;
 import com.project.freelecspringbootwebservice.web.dto.PostSaveRequestDto;
+import com.project.freelecspringbootwebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,8 +14,15 @@ public class PostsApiController {
     //생성자 주입방식을 권장하므로 final로 선언하고 RequireArgsConstructor 어노테이션으로 Bean객체를 받도록 함
     private final PostsService postsService;
 
+    //등록
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    //수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
     }
 }
